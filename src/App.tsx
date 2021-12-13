25b73830035143bb0599ae51eb25428ef2e85781
Maria22 <m.ushakova22@gmail.com>
2021-12-13 21:04:08 +0100

    add animation to topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Contact from './components/Contact/Contact';
 import Intro from './components/Intro/Intro';
 import Portfolio from './components/Portfolio/Portfolio';
@@ -6,9 +7,10 @@ import Works from './components/Works/Works';
 import style from './App.module.scss';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
   return (
     <div className={style.app}>
-      <Topbar />
+      <Topbar active={menuOpen} setActive={setMenuOpen} />
       <div className={style.sections}>
         <Intro />
         <Portfolio />
--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,23 +1,36 @@
 import style from './Topbar.module.scss'
 import { Person, Mail } from '@material-ui/icons';
+import React, { Dispatch, SetStateAction } from 'react';
 
-export default function Topbar() {
+interface Props {
+  active: boolean,
+  setActive: Dispatch<SetStateAction<boolean>>
+}
+const Topbar: React.FC<Props> = ({ active, setActive }) => {
+  const classNameTopbar = active ? style.active : style.topbar;
   return (
-    <div className={style.topbar}>
+    <div className={classNameTopbar}>
       <div className={style.wrapper}>
         <div className={style.left}>
           <a className={style.logo} href="#intro">MASHAUSHAKOVA</a>
-          <div className="item__container">
-            <Person />
+          <div className={style.item__container}>
+            <Person className={style.icon} />
             <span> +48 729 402 417</span>
           </div>
-          <div className="item__container">
-            <Mail />
+          <div className={style.item__container}>
+            <Mail className={style.icon} />
             <span>m.ushakova22@gmail.com</span>
           </div>
         </div>
-        <div className={style.right}></div>
+        <div className={style.right}>
+          <div className={style.hamburger} onClick={() => setActive(!active)}>
+            <span className={style.line__one}></span>
+            <span className={style.line__two}></span>
+            <span className={style.line__three}></span>
+          </div>
+        </div>
       </div>
     </div>
   )
 }
+export default Topbar;
