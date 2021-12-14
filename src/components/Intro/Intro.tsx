2c2cecafd6ce090f13e949eb2258c77fdabacbda
Maria22 <m.ushakova22@gmail.com>
2021-12-14 20:35:32 +0100

    feat: add text typing animation

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,6 +1,21 @@
-import style from './Intro.module.scss'
+import { useEffect, useRef } from 'react';
+import style from './Intro.module.scss';
+import { init } from 'ityped';
+
 
 const Intro: React.FC = () => {
+  const textRef = useRef<HTMLSpanElement>(null);
+  useEffect(() => {
+    if (!textRef.current) {
+      return;
+    }
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: [" Front-end Developer", " React Developer"]
+    });
+  }, []);
   return (
     <div className={style.intro} id='intro'>
       <div className={style.left}>
@@ -12,13 +27,14 @@ const Intro: React.FC = () => {
         <div className={style.wrapper}>
           <h2>Hi There, I'm</h2>
           <h1>Mariia Ushakova</h1>
-          <h3>Freelance</h3>
+          <h3>Freelance
+            <span ref={textRef}></span>
+          </h3>
           <span></span>
           <a href="#portfolio">
             <img src="assets/down.png" alt="Down button" />
           </a>
         </div>
-
       </div>
     </div>
   )
